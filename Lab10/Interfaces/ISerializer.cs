2dfb9c34d20fa0ca09eda15519872151d56bf764
dllpsk <megacooldude007@gmail.com>
2026-05-16 12:22:28 +0300

    Add files via upload

---
+++ Lab10/Interfaces/ISerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public interface ISerializer<T> where T: Lab9.Purple.Purple
+    {
+        T Deserialize();
+        void Serialize(T obj);
+    }
+}
