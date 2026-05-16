86c1c1b636892d70c5b4407d98e3b711c5232794
dllpsk <megacooldude007@gmail.com>
2026-05-16 12:19:28 +0300

    Add files via upload

---
+++ Lab10/Interfaces/IFileLifeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public interface IFileLifeController
+    {
+        void CreateFile();
+        void DeleteFile();
+
+        void EditFile(string text);
+        void ChangeFileExtension(string extension);
+    }
+}
---
+++ Lab10/Interfaces/IFileManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public interface IFileManager
+    {
+        string FolderPath { get; }
+        string FileName { get; }
+        string FileExtension { get; }
+        string FullPath { get; }
+
+        void SelectFolder(string path);
+        void ChangeFileName(string name);
+        void ChangeFileFormat(string format);
+    }
+}
